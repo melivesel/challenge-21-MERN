6eb97155c02ec4fd21aa95dea3c4120c26f7057e
Melissa Bentley <melivesel@gmail.com>
2024-09-01 21:16:37 -0500

    updated utils for graphql, converted controller to resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,70 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // Define query resolvers here
+  },
+  Mutation: {
+    createUser: async (parent, args, context, info) => {
+      const { body } = args;
+      const user = await User.create(body);
+
+      if (!user) {
+        throw new Error('Something went wrong while creating the user!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, args, context, info) => {
+      const { body } = args;
+      const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+
+      if (!user) {
+        throw new Error("User not found!");
+      }
+
+      const correctPw = await user.isCorrectPassword(body.password);
+
+      if (!correctPw) {
+        throw new Error('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    saveBook: async (parent, args, context, info) => {
+      const { user, body } = args;
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      } catch (err) {
+        throw new Error(err.message);
+      }
+    },
+
+    deleteBook: async (parent, args, context, info) => {
+      const { user, params } = args;
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        throw new Error("User not found!");
+      }
+
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const typeDefs = `
+type Query {
+  me: User
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(input: BookInput!): User
+  removeBook(bookId: ID!): User
+}
+
+type User {
+  _id: ID
+  username: String
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: ID
+  authors: [String]
+  description: String
+  title: String
+  image: String
+  link: String
+}
+
+type Auth {
+  token: String
+  user: User
+}
+
+input BookInput {
+  authors: [String]
+  description: String
+  title: String
+  bookId: ID
+  image: String
+  link: String
+}`
+module.exports = typeDefs;
